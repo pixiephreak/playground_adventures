41d87abef6325b1b62b2859cf82e12259823d768
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-13 23:38:35 -0400

    move firesbase to server

--- public/assets/js/fb_config.js
+++ public/assets/js/fb_config.js
@@ -9,8 +9,8 @@ var config = {
 
 firebase.initializeApp(config);
 
-// apiKey:AIzaSyDTrEvmBOIAoS0kMWQt1goNozOBUE1oeEI
-// authDomain: "playground-adventures.firebaseapp.com",
-// databaseURL: "https://playground-adventures.firebaseio.com",
-// storageBucket: "playground-adventures.appspot.com",
-// messagingSenderId: "382666633943"
+// apiKey=AIzaSyDTrEvmBOIAoS0kMWQt1goNozOBUE1oeEI
+// authDomain=playground-adventures.firebaseapp.com
+// databaseURL=https://playground-adventures.firebaseio.com
+// storageBucket=playground-adventures.appspot.com
+// messagingSenderId=382666633943
